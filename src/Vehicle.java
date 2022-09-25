347c371cca36d811c155b40824bfbcf4645c3ee3
tngu0329 <tngu0329@student.monash.edu>
2022-09-25 19:25:19 +0700

    Break #1

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,12 +1,50 @@
+// Sources:
+// 1) https://www.javatpoint.com/java-string-max-size
+
 public class Vehicle {
 
-    // instance variables
+    // Instance variables, declared as private by default
+    char[] regoNumber;
+    float mileage; // Distance in KM
 
-    // constructor
+    // Default Constructor
+    public Vehicle() {
+        regoNumber = new char[6]; // For memory efficiency, a char array of exact size 6 is used instead of a String data type
+        mileage = 0.0f;
+    }
 
-    //getter
+    public char[] getRegoNumber() {
+        return regoNumber;
+    }
 
-    //setter
+    // Getters
+
+    public void setRegoNumber(char[] regoNumber) {
+        this.regoNumber = regoNumber;
+    }
+
+    public float getMileage() {
+        return mileage;
+    }
+
+    // Setters
+    public void setMileage(float mileage) {
+        this.mileage = mileage;
+    }
+
+    public enum VehicleType {
+        Sedan,
+        SUV,
+        Van,
+        Minibus
+    }
 
     //toString method
+//    @Override
+//    public String toString() {
+//        return "Vehicle{" +
+//                "Rego Number: " + Arrays.toString(regoNumber) +
+//                ", mileage=" + mileage +
+//                '}';
+//    }
 }
